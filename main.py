5473b982a3f26020b6a4c4a2089365a55415edb6
Nelson Chu <nelson@high5.ai>
2022-04-10 09:35:26 +0800

    clean up command

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ import os
 
 
 def job(num_gpus, base_model, train_file, validation_file, finetuned_id, num_train_epochs, gradient_accumulation_steps, per_device_train_batch_size):
-    command ='''deepspeed --num_gpus={} train.py
-    --deepspeed ds_config.json
-    --model_name_or_path {}
-    --train_file {}
-    --validation_file {}
-    --do_train
-    --do_eval
-    --fp16
-    --overwrite_cache
-    --evaluation_strategy="steps"
-    --output_dir jobs\{}\output
-    --num_train_epochs {}
-    --eval_steps 15
-    --gradient_accumulation_steps {}
-    --per_device_train_batch_size {}
-    --use_fast_tokenizer False
-    --learning_rate 5e-06
-    --warmup_steps 10
-    '''.format(num_gpus, base_model, train_file, validation_file, finetuned_id, num_train_epochs, gradient_accumulation_steps, per_device_train_batch_size)
+    command ='''deepspeed --num_gpus={} train.py \
+    --deepspeed ds_config.json \
+    --model_name_or_path {} \
+    --train_file {} \
+    --validation_file {} \
+    --do_train \
+    --do_eval \
+    --fp16 \
+    --overwrite_cache \
+    --evaluation_strategy="steps" \
+    --output_dir jobs\{}\output \
+    --num_train_epochs {} \
+    --eval_steps 15 \
+    --gradient_accumulation_steps {} \
+    --per_device_train_batch_size {} \
+    --use_fast_tokenizer False \
+    --learning_rate 5e-06 \
+    --warmup_steps 10'''.format(num_gpus, base_model, train_file, validation_file, finetuned_id, num_train_epochs, gradient_accumulation_steps, per_device_train_batch_size)
     print('Running Training Job........................')
     print(command)
     os.system(command)
